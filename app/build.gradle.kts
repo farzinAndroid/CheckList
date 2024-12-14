7b892acfee599b94421a75eea739a7a53e065ec9
Farzin <farzinallahyari1234@gmail.com>
2024-12-14 13:01:29 +0330

    fixed notification in android 13 and above. updated gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.farzin.checklisttodo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
